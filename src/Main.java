d2355663ff6996aeef56701ee1ed63dd437ed440
IgorChijov <igorchijof@gmail.com>
2024-05-02 16:18:55 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i < 11; i++){
+            System.out.println(i);
+        }
+
+        for (int i = 10; i > 0; i--){
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 17; i = i + 2){
+            System.out.println(i);
+        }
+
+        for (int i = 10; i > -11; i--){
+            System.out.println(i);
+        }
+
+        for (int i = 1904; i < 2096; i = i + 4){
+            System.out.println(i + " год является високосным");
+        }
+
+        for (int i = 7; i < 99; i = i + 7){
+            System.out.println(i);
+        }
+
+        for (int i = 1; i < 513; i = i * 2){
+            System.out.println(i);
+        }
+
+        int moneyEveryMonth = 29000;
+        int total = 0;
+        int month = 0;
+        for (float i = 0; i < 12; i++){
+            total = total + total / 100;
+            total = total + moneyEveryMonth;
+            month++;
+            System.out.println("Месяц " + month + " сумма накоплений равна " + total + " рублей");
+        }
+
+        int two = 2;
+        for (int i = 1;i < 11; i++){
+            System.out.println(" 2 * " + i + " = " + two * i );
+        }
+
     }
 }
